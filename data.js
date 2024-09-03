c87d1f9f09e440a9678a381506380b60b3c64936
Habbo Chabbo <habbo2001chabbo@gmail.com>
2024-09-04 00:49:19 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,5 @@
+const Examples_Table = [
+    { description: "create virtual envirement on 'path'", code: "python -m venv path" },
+    { description: "sfijefcueif", code: "python -m venv path\nsfierfi8o\nfj89ferf" },
+    
+];
