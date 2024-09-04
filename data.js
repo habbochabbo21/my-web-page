2d5b3a8bbafe7f64af928bec75c13bc621b8202f
Habbo Chabbo <84484768+habbochabbo21@users.noreply.github.com>
2024-09-04 01:05:55 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,8 @@
 const Examples_Table = [
     { description: "create virtual envirement on 'path'", code: "python -m venv path" },
-    { description: "Activating virtual envirement in Linux", code: "source path/bin/activate" },
+    { description: "Activating virtual envirement in Linux/Mac", code: "source path/bin/activate" },
+    { description: "Deactivating virtual envirement in Linux/Mac", code: "deactivate" },
+    { description: "Activating virtual envirement in Windows", code: "path/Scripts/activate.bat" },
+    { description: "Deactivating virtual envirement in Windows", code: "path/Scripts/deactivate.bat" },
     
 ];
